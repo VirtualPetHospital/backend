44c5f34aeea3450a5c95a66267d76240741621df
astarforbae <10215101528@stu.ecnu.edu.cn>
2024-04-03 16:52:02 +0800

    fix file

--- file-service/src/main/java/cn/vph/files/FileServiceApplication.java
+++ file-service/src/main/java/cn/vph/files/FileServiceApplication.java
@@ -2,15 +2,9 @@ package cn.vph.files;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.multipart.MultipartResolver;
 
 @SpringBootApplication
 public class FileServiceApplication {
-    @Bean(name = "multipartResolver")
-    public MultipartResolver multipartResolver() {
-        return new CustomMultipartResolver();
-    }
     public static void main(String[] args) {
         SpringApplication.run(FileServiceApplication.class, args);
     }
