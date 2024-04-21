f8f5b32f4fae7804a22ec0339b8e046e00fb2766
astarforbae <10215101528@stu.ecnu.edu.cn>
2024-04-21 22:22:00 +0800

    fix

--- file-service/src/main/java/cn/vph/files/common/CustomMultipartResolver.java
+++ file-service/src/main/java/cn/vph/files/common/CustomMultipartResolver.java
@@ -5,7 +5,6 @@ import org.apache.commons.fileupload.FileUpload;
 import org.apache.commons.fileupload.FileUploadBase;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartException;
@@ -22,12 +21,7 @@ import java.util.List;
  **/
 @Component
 public class CustomMultipartResolver extends CommonsMultipartResolver {
-    @Autowired
-    private FileUploadProgressListener progressListener;
 
-    public void setFileUploadProgressListener(FileUploadProgressListener progressListener) {
-        this.progressListener = progressListener;
-    }
 
     @Override
     public MultipartParsingResult parseRequest(HttpServletRequest request) throws MultipartException {
@@ -35,8 +29,6 @@ public class CustomMultipartResolver extends CommonsMultipartResolver {
         FileUpload fileUpload = prepareFileUpload(encoding);
         // fileUpload.setFileSizeMax(1024 * 1024 * 500);// 单个文件最大500M
         // fileUpload.setSizeMax(1024 * 1024 * 500);// 一次提交总文件最大500M
-        progressListener.setSession(request.getSession());// 问文件上传进度监听器设置session用于存储上传进度
-        fileUpload.setProgressListener(progressListener);// 将文件上传进度监听器加入到 fileUpload 中
         try {
             List<FileItem> fileItems = ((ServletFileUpload) fileUpload).parseRequest(request);
             return parseFileItems(fileItems, encoding);
